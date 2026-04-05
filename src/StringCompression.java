df3b9333f743be180b1e271f974370b61d5955a3
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-04-06 01:40:39 +0530

    Update StringCompression.java

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -7,21 +7,31 @@ public class StringCompression {
 
         String S = sc.nextLine();
 
-        String result = "";
+        if (S.length() == 0) {
+            System.out.println(S);
+            return;
+        }
+
+        StringBuilder result = new StringBuilder();
         int count = 1;
 
-        for (int i = 1; i <= S.length(); i++) {
-            if (i < S.length() && S.charAt(i) == S.charAt(i - 1)) {
+        for (int i = 1; i < S.length(); i++) {
+            if (S.charAt(i) == S.charAt(i - 1)) {
                 count++;
             } else {
-                result += S.charAt(i - 1);
-                result += count;
+                result.append(S.charAt(i - 1));
+                result.append(count);
                 count = 1;
             }
         }
 
+        // last character
+        result.append(S.charAt(S.length() - 1));
+        result.append(count);
+
+        // final check
         if (result.length() < S.length()) {
-            System.out.println(result);
+            System.out.println(result.toString());
         } else {
             System.out.println(S);
         }
@@ -29,3 +39,5 @@ public class StringCompression {
         sc.close();
     }
 }
+
+
